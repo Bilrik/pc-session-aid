43c6718a73707b8e39054450f1ef91174a155be6
Christian Howell <chowell@firstorion.com>
2023-12-14 21:40:28 -0600

    added rest of races

--- pkg/Race/types.go
+++ pkg/Race/types.go
@@ -1,29 +1,62 @@
 package race
 
-import stats "github.com/Bilrik/pc-session-aid/pkg/Stats"
+import (
+	class "github.com/Bilrik/pc-session-aid/pkg/Class"
+	stats "github.com/Bilrik/pc-session-aid/pkg/Stats"
+)
 
 type Race struct {
-	Name  string
-	Size  stats.Size
-	Speed uint
+	Name         string
+	Size         stats.Size
+	Speed        uint
+	favoredClass class.Class
 }
 
 var (
-	Elf = Race{
-		Name:  "Elf",
+	Human = Race{
+		Name:  "Human",
 		Size:  stats.Medium,
 		Speed: 30,
 	}
 
 	Dwarf = Race{
-		Name:  "Dwarf",
-		Size:  stats.Medium,
-		Speed: 20,
+		Name:         "Dwarf",
+		Size:         stats.Medium,
+		Speed:        20,
+		favoredClass: class.Fighter,
+	}
+
+	Elf = Race{
+		Name:         "Elf",
+		Size:         stats.Medium,
+		Speed:        30,
+		favoredClass: class.Wizard,
 	}
 
 	Gnome = Race{
-		Name:  "Gnome",
-		Size:  stats.Small,
-		Speed: 20,
+		Name:         "Gnome",
+		Size:         stats.Small,
+		Speed:        20,
+		favoredClass: class.Bard,
+	}
+
+	HalfElf = Race{
+		Name:  "Half-Elf",
+		Size:  stats.Medium,
+		Speed: 30,
+	}
+
+	HalfOrc = Race{
+		Name:         "Half-Orc",
+		Size:         stats.Medium,
+		Speed:        30,
+		favoredClass: class.Barbarian,
+	}
+
+	Halfling = Race{
+		Name:         "Halfling",
+		Size:         stats.Small,
+		Speed:        20,
+		favoredClass: class.Rogue,
 	}
 )
